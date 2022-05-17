bd660338c22a4643f2ec7be26241ca4b00dd4c34
Maria Emilia <mariaemiliapgmartins@hotmail.com>
2022-05-17 17:01:48 -0300

    ESLint

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -1,21 +1,22 @@
-import outsideClick from "./outsideClick.js";
+import outsideClick from "./outsideclick.js";
 
 export default function initMenuMobile() {
   const menuButton = document.querySelector('[data-menu="button"]');
   const menuList = document.querySelector('[data-menu="list"]');
   const eventos = ["click", "touchstart"];
 
-  if (menuButton) {
-    function openMenu(event) {
-      menuList.classList.add("active");
-      menuButton.classList.add("active");
-      outsideClick(menuList, eventos, () => {
-        console.log("teste");
+  function openMenu() {
+    menuList.classList.add("active");
+    menuButton.classList.add("active");
+    outsideClick(menuList, eventos, () => {
+      console.log("teste");
 
-        menuList.classList.remove("active");
-        menuButton.classList.remove("active");
-      });
-    }
+      menuList.classList.remove("active");
+      menuButton.classList.remove("active");
+    });
+  }
+
+  if (menuButton) {
     eventos.forEach((evento) => menuButton.addEventListener(evento, openMenu));
   }
 }
